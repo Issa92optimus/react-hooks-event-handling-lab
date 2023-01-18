db604c464d35f5980a93cb20be310346ad92cec5
Issa Nyale <me@email.com>
2023-01-18 10:25:25 +0300

    Completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    return (
+        <button onFocus={focus => console.log('Good!')} onBlur={blur => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
